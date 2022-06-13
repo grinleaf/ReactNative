46781a4f83b6ce742379ef735fc407ad80e8932c
grinleaf <grinleaf774@gmail.com>
2022-06-13 15:03:53 +0900

    Ex02ButtonEvent

---
+++ Ex02ButtonEvent/MainComponent.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import {View, Text, Button} from 'react-native'
+
+class MainComponent extends Component{
+    render(){
+        return(
+            <View>
+                <Text>Hello~!</Text>
+                <Button title='button'></Button>
+            </View>
+        )
+    }
+}
+
+export default MainComponent
